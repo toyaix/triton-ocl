a0030a59bfbb5cea8beef1ed080b73b911bed576
BobHuang <git@bobhuang.xyz>
2025-06-10 08:04:52 +0800

    [SPIRV] add matmul case

---
+++ python/tutorials/spirv_demo/02-matrix-multiplication.py
@@ -0,0 +1,81 @@
+import triton
+import triton.language as tl
+
+@triton.jit
+def matmul_kernel(
+    a_ptr, b_ptr, c_ptr,
+    M, N, K,
+    stride_am, stride_ak,
+    stride_bk, stride_bn,
+    stride_cm, stride_cn,
+    BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr
+):
+    a_ptr = a_ptr.to(tl.pointer_type(tl.float32))
+    b_ptr = b_ptr.to(tl.pointer_type(tl.float32))
+    c_ptr = c_ptr.to(tl.pointer_type(tl.float32))
+    pid_n = tl.program_id(axis=0)
+    pid_m = tl.program_id(axis=1)
+
+    offs_m = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_n = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+
+    # initialize pointers for a and b
+    a_ptrs = a_ptr + offs_m[:, None] * stride_am
+    b_ptrs = b_ptr + offs_n[None, :] * stride_bn
+
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+
+    # accumulate along the N dimension
+    for k in range(K):
+        # load current blocks of a and b with boundary check
+        a = tl.load(a_ptrs)
+        b = tl.load(b_ptrs)
+        accumulator += a * b
+        a_ptrs += stride_ak
+        b_ptrs += stride_bk
+
+    # write result back to c
+    c_ptrs = c_ptr + offs_m[:, None] * stride_cm + offs_n[None, :] * stride_cn
+    offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+    tl.store(c_ptrs, accumulator, mask=c_mask)
+
+
+def matmul(a, b):
+    # Check constraints.
+    assert a.shape[1] == b.shape[0], "Incompatible dimensions"
+    assert a.is_contiguous(), "Matrix A must be contiguous"
+    M, K = a.shape
+    K, N = b.shape
+    # Allocates output.
+    c = torch.empty((M, N), device=a.device, dtype=torch.float32)
+    # 1D launch kernel where each block gets its own program.
+    grid = lambda META: (triton.cdiv(N, META['BLOCK_SIZE_N']), triton.cdiv(M, META['BLOCK_SIZE_M']), )
+    matmul_kernel[grid](
+        a, b, c,
+        M, N, K,
+        a.stride(0), a.stride(1),
+        b.stride(0), b.stride(1),
+        c.stride(0), c.stride(1),
+        BLOCK_SIZE_M=16,
+        BLOCK_SIZE_N=16,
+    )
+    return c
+
+
+
+import torch
+
+DEVICE = "cpu"
+
+torch.manual_seed(0)
+a = torch.randn((128, 256), device=DEVICE, dtype=torch.float32)
+b = torch.randn((256, 512), device=DEVICE, dtype=torch.float32)
+torch_output = torch.matmul(a, b)
+triton_output = matmul(a, b)
+
+if torch.allclose(triton_output, torch_output, atol=1e-2, rtol=0):
+    print("✅ Triton and Torch match")
+else:
+    print("❌ Triton and Torch differ")
